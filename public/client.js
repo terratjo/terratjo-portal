49d51888ba201a6614a41ed696d811145326de6f
Terratjo Deploy <deploy@terratjo.com>
2026-07-08 14:53:54 +0700

    Move Today button inside calendar container footer (bottom-left, calendar page only)

--- public/client.js
+++ public/client.js
@@ -637,7 +637,6 @@ $('btn-goto-today')?.addEventListener('click', () => {
   const now = new Date(_todayJkt + 'T00:00:00');
   calMonth = now.getMonth();
   calYear = now.getFullYear();
-  navigate('calendar');
   renderCalendar();
 });
 
